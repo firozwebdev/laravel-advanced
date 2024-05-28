2e65a966c52c75538a674261112dc393161a8b5a
Sabuz <everythingkst@gmail.com>
2024-05-28 21:36:37 +0600

    currency converter added

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use RuntimeException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +19,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/sync/invoices/unpaid', function(Request $request) {
+
+    $rates = Cache::remember('currency_rates', now()->addHours(12), function() {
+        $response = Http::withOptions([
+        'verify' => 'C:/certificates/cacert.pem',
+        ])->get('https://api.currencyapi.com/v3/latest', [
+            'apikey' => 'cur_live_BaUSTWrL9z6vWPRCt1YK0xGKG665sIhPBq2dqwKm',
+            'currencies' => 'EUR,USD,CAD'
+        ]);
+        if($response->ok()) {
+            $rates =  $response->json()['data'];
+            return $rates;
+        }else{
+            throw new RuntimeException('Currency Api Error');
+        }
+
+    });
+    
+
+    return [
+        ['id' => 1, 'client_id' => 3, 'currency' => 'CAD', 'amount' => round($rates['CAD']['value'] * 100,2)],
+        ['id' => 4, 'client_id' => 6, 'currency' => 'EUR', 'amount' => round($rates['EUR']['value'] * 200,2)],
+        ['id' => 5, 'client_id' => 2, 'currency' => 'USD', 'amount' => round($rates['EUR']['value'] * 150,2)],
+    ];
 });
